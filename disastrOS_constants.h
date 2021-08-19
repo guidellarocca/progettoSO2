a3e7f70dc6489ed255dc5b2844a6a1f7a8db593d
guidellarocca <guidellarocca@gmail.com>
2021-08-20 01:41:01 +0200

    disastOS_constants.h

--- disastrOS_constants.h
+++ disastrOS_constants.h
@@ -5,6 +5,12 @@
 #define MAX_NUM_RESOURCES_PER_PROCESS 32
 #define MAX_NUM_DESCRIPTORS_PER_PROCESS 32
 
+// messages ***************************************
+#define MAX_TEXT_LEN                        255         // dimensione max del messaggio (se si imposta < 6 è possibile testare errori)
+#define MAX_NUM_MESSAGES                    1024
+#define MAX_NUM_MESSAGES_PER_MSG_QUEUE      1           // num max di messaggi nella coda (modificare per testare coda piena)
+#define MAX_NUM_PRIORITIES                  10          // num di subqueue della msg queue (modificare per testare vari errori)
+
 #define STACK_SIZE        16384
 // signals
 #define MAX_SIGNALS 32
@@ -26,6 +32,20 @@
 #define DSOS_ERESOURCECLOSE -12
 #define DSOS_ERESOURCEINUSE -13
 
+//**************************************************************************
+#define DSOS_EMQ_NOFD       -14
+#define DSOS_EMQ_NOEXIST    -15
+
+// errors msg queue
+#define DSOS_EMQ_CREATE     -16
+#define DSOS_EMQ_READ       -17
+#define DSOS_EMQ_WRITE      -18
+#define DSOS_EMQ_CLOSE      -19
+#define DSOS_EMQ_UNLINK     -20
+#define DSOS_EMQ_WAIT       -21
+
+//*****************************************************************************
+
 // syscall numbers
 #define DSOS_MAX_SYSCALLS 32
 #define DSOS_MAX_SYSCALLS_ARGS 8
@@ -39,6 +59,18 @@
 #define DSOS_CALL_CLOSE_RESOURCE 8
 #define DSOS_CALL_DESTROY_RESOURCE 9
 #define DSOS_CALL_SHUTDOWN  10
+
+//********************************************************************************
+
+// syscall numbers msg queue
+#define DSOS_CALL_MQ_CREATE     11
+#define DSOS_CALL_MQ_OPEN       12
+#define DSOS_CALL_MQ_CLOSE      13
+#define DSOS_CALL_MQ_UNLINK     14
+#define DSOS_CALL_MQ_READ       15
+#define DSOS_CALL_MQ_WRITE      16
+
+//********************************************************************************
 
 //resources
 #define DSOS_CREATE 0x1
